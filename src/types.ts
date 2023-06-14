0e711316215143b4939f0aec30fcb3b9a4e86eff
程茂宇 <chengmaoyu@100.me>
2023-06-14 17:45:32 +0800

    替换模型

--- src/types.ts
+++ src/types.ts
@@ -13,7 +13,7 @@ export interface ChatMessage {
 }
 
 export type Role = "system" | "user" | "assistant" | "error"
-export type Model = "gpt-3.5-turbo" | "gpt-4" | "gpt-4-32k"
+export type Model = "gpt-3.5-turbo-0613" | "gpt-4" | "gpt-4-32k"
 
 export interface Prompt {
   desc: string
